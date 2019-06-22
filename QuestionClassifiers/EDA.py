4a2d9667d25e039e28d0c0af3c680939fa959ad3
NolangerAtEase <esser.nolan@gmail.com>
2019-06-22 18:08:06 -0400

    moved non-EDA stuff to another file

--- QuestionClassifiers/EDA.py
+++ QuestionClassifiers/EDA.py
@@ -86,12 +86,6 @@ def clean_text(text):
 qs['CleanText'] = qs['Text'].apply(clean_text)
 
 
-# Look at 50 or so mostfght take a while)
-#
-# split() returns list of all the words in the string
-
-
-
 
 #Use this way:
 allTxt = qs['Text'].to_string()
@@ -119,80 +113,6 @@ most_occur = c.most_common(1000)
 c.clear()
 x, y = zip(*most_occur)
 plt.scatter(np.log(range(1,1001)),np.log(y))
-
-#Look at SVD to see if we see some clustering on regular tdf
-
-vec = CountVectorizer()
-text_counts = vec.fit_transform(qs['CleanText'][:10000])
-
-svd = TruncatedSVD(n_components = 50, random_state = 1, n_iter = 20)
-svd.fit(text_counts)
-
-
-print(svd.explained_variance_ratio_)
-print(svd.explained_variance_ratio_.sum())
-
-svd_result = svd.fit_transform(text_counts)
-
-df = pd.DataFrame()
-df['svd-one'] = svd_result[:,0]
-df['svd-two'] = svd_result[:,1]
-df['svd-three'] = svd_result[:,2]
-
-print('Explained variation per principal component: {}'.format(svd.explained_variance_ratio_))
-
-sns.scatterplot(x = "svd-one", y= "svd-two", hue = qs['Class'][:1000], data = df)
-
-
-tsne = TSNE(n_components = 2, verbose = 1)
-tsne_results = tsne.fit_transform(df)
-
-
-df2 = pd.DataFrame()
-df2['tsne-2d-one'] = tsne_results[:,0]
-df2['tsne-2d-two'] = tsne_results[:,1]
-
-sns.scatterplot(x = "tsne-2d-one", y = "tsne-2d-two", hue = qs['Class'][:1000], data = df2)
-
-
-
-
-
-# Do it for tf-idf
-tfidf_vec = TfidfVectorizer()
-
-tfidfMat = tfidf_vec.fit_transform(qs['CleanText'])
-n_topics = 9
-
-lsa_model = TruncatedSVD(n_components = n_topics)
-lsa_topic_matrix = lsa_model.fit_transform(tfidfMat)
-
-
-
-
-
-
-def get_keys(topic_matrix):
-    '''
-    returns an integer list of predicted topic 
-    categories for a given topic matrix
-    '''
-    keys = topic_matrix.argmax(axis=1).tolist()
-    return keys
-
-def keys_to_counts(keys):
-    '''
-    returns a tuple of topic categories and their 
-    accompanying magnitudes for a given list of keys
-    '''
-    count_pairs = Counter(keys).items()
-    categories = [pair[0] for pair in count_pairs]
-    counts = [pair[1] for pair in count_pairs]
-    return (categories, counts)
-    
-lsa_keys = get_keys(lsa_topic_matrix)
-lsa_categories, lsa_counts = keys_to_counts(lsa_keys)
-
 
 
 
